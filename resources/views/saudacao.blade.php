73250cf732390d6c133df8bcf9e6b241a6cb8d56
php 19 <php.19@catatau.cec.ime.usp.br>
2018-01-30 21:44:45 -0200

    primeiro commit

---
+++ resources/views/saudacao.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Saudação Marota</title>
+  </head>
+  <body>
+    @for ($i=0; $i < 10; $i++)
+      <h1>Olá {{$i}} </h1>
+    @endfor
+  </body>
+</html>
